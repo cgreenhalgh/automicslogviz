9154824e9162cc9b4b4a8e248e662dbd21ce93d9
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2011-07-25 13:17:07 +0100

    added timeline time-range output options, hours by default

--- src/automicslogviz/DrawTimelines.java
+++ src/automicslogviz/DrawTimelines.java
@@ -22,6 +22,7 @@ package automicslogviz;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -43,8 +44,8 @@ public class DrawTimelines {
 	public static float SCALE = 1000;
 	public static float BIG_MARGIN = 100;
 	public static float SMALL_MARGIN = 10;
-	public static double MIN_HOD = 8; // 8?
-	public static double MAX_HOD = 16;
+	public static double MIN_MIN_HOD = 8; // 8?
+	public static double MAX_MAX_HOD = 16;
 	private static final int LANE_WIDTH = 80;
 	private static final String NONE = "none";
 	static final String DASHED = "dashed";
@@ -56,8 +57,8 @@ public class DrawTimelines {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if (args.length!=3) {
-			logger.log(Level.SEVERE, "usage: <logdir> <zonefile> <outdir>");
+		if (args.length!=3 && args.length!=5 && args.length!=6) {
+			logger.log(Level.SEVERE, "usage: <logdir> <zonefile> <outdir> [min-hod max-hod [group]]");
 			System.exit(-1);			
 		}
 		try {
@@ -69,74 +70,134 @@ public class DrawTimelines {
 				logger.log(Level.SEVERE, "Output directory not valid: "+outdir);
 				System.exit(-1);
 			}
-			
+			if (args.length==3) {
+				writeTimeline(users, zones, outdir, MIN_MIN_HOD, MAX_MAX_HOD, null);
+				for (int hod=(int)Math.ceil(MIN_MIN_HOD); hod<MAX_MAX_HOD; hod++) {
+					writeTimeline(users, zones, outdir, hod, hod+1, null);			
+				}
+			}
+			else {
+				double MIN_HOD = parseFilenameTime(args[3]);
+				double MAX_HOD = parseFilenameTime(args[4]);
+				String group = null;
+				if (args.length>5) {
+					group = args[5];
+					for (int ui=0; ui<users.size(); ui++) {
+						 if (!group.equals(users.get(ui).getTrialid())) {
+							 users.remove(ui);
+							 ui--;
+						 }
+					}
+				}
+				writeTimeline(users, zones, outdir, MIN_HOD, MAX_HOD, group);
+			}
 //			svg = createBackgroundFile(new File(outdir,"allusers.svg"));
 //			drawPositions(svg, users, null, false);
 //			svg.close();
-			SvgFile svg;
-			svg = createTimelineFile(new File(outdir,"timelinetest.svg"));
 			
-			for (int hod=(int)Math.ceil(MIN_HOD); hod<MAX_HOD; hod++) {
-				double x = BIG_MARGIN+(SCALE-BIG_MARGIN-SMALL_MARGIN)*(hod-MIN_HOD)/(MAX_HOD-MIN_HOD);
-				svg.line((float)x, (float)SMALL_MARGIN, (float)x, (float)(SCALE-BIG_MARGIN), "#888", 0.5f, " stroke-dasharray=\"2 4\"");
-				svg.text(hod+":00", (float) x, SCALE-BIG_MARGIN+TEXT_HEIGHT, 11, "#000", null);
-			}
-			
-			int ui =0;
-			for (UserData user : users) {
-				Timeline tl = new Timeline();
-				float y = SMALL_MARGIN+LANE_WIDTH/2+LANE_WIDTH*ui;
-				TimelinePoint lasttp = new TimelinePoint(MIN_HOD, (float)(BIG_MARGIN), y);
-				boolean first = true;
-				boolean truncated = false;
-				for (Position p : user.getPositions()) {
-					TimelinePoint tp = new TimelinePoint(p.getHourOfDay(), (float)(BIG_MARGIN+(SCALE-BIG_MARGIN-SMALL_MARGIN)*(p.getHourOfDay()-MIN_HOD)/(MAX_HOD-MIN_HOD)), y);
-					if (lasttp==null)
-						lasttp = tp;
-					else {
-						if (first || !p.isTruncated()) {
-							TimelineSegment s1 =new TimelineSegment(lasttp, tp, first ? NONE : (truncated || p.isTruncated() ? DASHED : SOLID));
-							tl.getSegments().add(s1);
-							lasttp = tp;
-							truncated = p.isTruncated();
-							first = false;
-						}
-						else
-							truncated = true;
-					}
-				}
-				if (truncated && lasttp!=null) {
-					// last line
-					Position p = user.getPositions().get(user.getPositions().size()-1);
-					TimelinePoint tp = new TimelinePoint(p.getHourOfDay(), (float)(BIG_MARGIN+(SCALE-BIG_MARGIN-SMALL_MARGIN)*(p.getHourOfDay()-MIN_HOD)/(MAX_HOD-MIN_HOD)), y);
-					TimelineSegment s1 =new TimelineSegment(lasttp, tp, DASHED);
-					tl.getSegments().add(s1);					
-					lasttp = tp;
-				}
-				{ 
-					// last last line :-)
-					// last line
-					TimelinePoint tp = new TimelinePoint(MAX_HOD, (float)(SCALE-SMALL_MARGIN), y);
-					TimelineSegment s1 =new TimelineSegment(lasttp, tp, NONE);
-					tl.getSegments().add(s1);					
-				}
-				drawTimeline(svg, tl);
-				svg.text(user.getTrialid()+"/"+user.getTrialuserid(), 0, y, 11, "#000", null);
-				
-				for (Event e : user.getEvents()) {
-					// init==sys => notificationTs & acceptanceTime (seconds, 0==not)
-					// init==user => taskAcceptTs
-					if (!"newImages".equals(e.getTaskType()))
-						drawEvent(svg, e, tl);
-				}
-				
-				ui++;
-				
-			}
-			svg.close();
 		} catch (Exception e) {
 			logger.log(Level.SEVERE, "Error creating "+args[1], e);
 		}		
+	}
+	private static double parseFilenameTime(String ts) {
+		double t = 0;
+		if (ts.length()>=2) 
+			t += Integer.parseInt(ts.substring(0,2));
+		else if (ts.length()<2)
+			throw new RuntimeException("Invalid time "+ts+" should be HH[MM[SS]]");
+		if (ts.length()>=4)
+			t += (1.0/60)*Integer.parseInt(ts.substring(2,4));
+		else if (ts.length()==3)
+			throw new RuntimeException("Invalid time "+ts+" should be HH[MM[SS]]");
+		if (ts.length()==6)
+			t += (1.0/60/60)*Integer.parseInt(ts.substring(4,6));
+		else if (ts.length()==5 || ts.length()>6)
+			throw new RuntimeException("Invalid time "+ts+" should be HH[MM[SS]]");
+		return t;
+	}
+	private static void writeTimeline(List<UserData> users,
+			Map<Integer, Zone> zones, File outdir, double MIN_HOD,
+			double MAX_HOD, String group) throws UnsupportedEncodingException, FileNotFoundException {
+		// TODO Auto-generated method stub
+		SvgFile svg;
+		svg = createTimelineFile(new File(outdir,"timelinetest-"+(group==null ? "" : group+"-")+getFilenameTime(MIN_HOD)+"-"+getFilenameTime(MAX_HOD)+".svg"));
+		
+		for (int hod=(int)Math.ceil(MIN_HOD); hod<MAX_HOD; hod++) {
+			double x = BIG_MARGIN+(SCALE-BIG_MARGIN-SMALL_MARGIN)*(hod-MIN_HOD)/(MAX_HOD-MIN_HOD);
+			svg.line((float)x, (float)SMALL_MARGIN, (float)x, (float)(SCALE-BIG_MARGIN), "#888", 0.5f, " stroke-dasharray=\"2 4\"");
+			svg.text(hod+":00", (float) x, SCALE-BIG_MARGIN+TEXT_HEIGHT, 11, "#000", null);
+		}
+		
+		int ui =0;
+		for (UserData user : users) {
+			float y = SMALL_MARGIN+LANE_WIDTH/2+LANE_WIDTH*ui;
+			svg.text(user.getTrialid()+"/"+user.getTrialuserid(), 0, y, 11, "#000", null);
+			ui++;
+		}
+		startClip(svg);
+		ui =0;
+		for (UserData user : users) {
+			Timeline tl = new Timeline();
+			float y = SMALL_MARGIN+LANE_WIDTH/2+LANE_WIDTH*ui;
+			TimelinePoint lasttp = new TimelinePoint(MIN_HOD, (float)(BIG_MARGIN), y);
+			boolean first = true;
+			boolean truncated = false;
+			for (Position p : user.getPositions()) {
+				TimelinePoint tp = new TimelinePoint(p.getHourOfDay(), (float)(BIG_MARGIN+(SCALE-BIG_MARGIN-SMALL_MARGIN)*(p.getHourOfDay()-MIN_HOD)/(MAX_HOD-MIN_HOD)), y);
+				if (lasttp==null)
+					lasttp = tp;
+				else {
+					if (first || !p.isTruncated()) {
+						TimelineSegment s1 =new TimelineSegment(lasttp, tp, first ? NONE : (truncated || p.isTruncated() ? DASHED : SOLID));
+						tl.getSegments().add(s1);
+						lasttp = tp;
+						truncated = p.isTruncated();
+						first = false;
+					}
+					else
+						truncated = true;
+				}
+			}
+			if (truncated && lasttp!=null) {
+				// last line
+				Position p = user.getPositions().get(user.getPositions().size()-1);
+				TimelinePoint tp = new TimelinePoint(p.getHourOfDay(), (float)(BIG_MARGIN+(SCALE-BIG_MARGIN-SMALL_MARGIN)*(p.getHourOfDay()-MIN_HOD)/(MAX_HOD-MIN_HOD)), y);
+				TimelineSegment s1 =new TimelineSegment(lasttp, tp, DASHED);
+				tl.getSegments().add(s1);					
+				lasttp = tp;
+			}
+			{ 
+				// last last line :-)
+				// last line
+				TimelinePoint tp = new TimelinePoint(MAX_HOD, (float)(SCALE-SMALL_MARGIN), y);
+				TimelineSegment s1 =new TimelineSegment(lasttp, tp, NONE);
+				tl.getSegments().add(s1);					
+			}
+			drawTimeline(svg, tl);
+			
+			for (Event e : user.getEvents()) {
+				// init==sys => notificationTs & acceptanceTime (seconds, 0==not)
+				// init==user => taskAcceptTs
+				if (!"newImages".equals(e.getTaskType()))
+					drawEvent(svg, e, tl);
+			}
+			
+			ui++;
+			
+		}
+		endClip(svg);
+		svg.close();
+
+	}
+	private static DecimalFormat df2 = new DecimalFormat("00");
+	private static String getFilenameTime(double hod) {
+		hod += 0.5/60/60;
+		int hours = (int)(Math.floor(hod));
+		double moh = (hod-hours)*60;
+		int minutes = (int)(Math.floor(moh));
+		double som = (moh-minutes)*60;
+		int seconds = (int)(Math.floor(som));
+		return df2.format(hours)+df2.format(minutes)+df2.format(seconds);
 	}
 	static void drawEvent(SvgFile svg, Event e, Timeline tl) {
 		String stroke = getEventStroke(e);
@@ -239,9 +300,9 @@ public class DrawTimelines {
 		else if ("Q-Zone".equals(e.getTaskType()) || "annotate".equals(e.getTaskType()))
 			fill = "#f00";
 		else if ("end of ride".equals(e.getTaskType()))
-			fill = "#ff0";
+			fill = "#aa0";
 		else if ("photo".equals(e.getTaskType()) || "newImage".equals(e.getTaskType()))			
-			fill = "#f0f";
+			fill = "#a0a";
 		return fill;
 	}
 	private static TimelineSegment getTimelineSegment(Timeline tl, double time) {
@@ -279,6 +340,16 @@ public class DrawTimelines {
 		svg.desc("DrawTimelines of automics data, "+new Date());
 		svg.rect(BIG_MARGIN, SMALL_MARGIN, SCALE-BIG_MARGIN-SMALL_MARGIN, SCALE-BIG_MARGIN-SMALL_MARGIN, "#888", 0.5f, "#fff");
 		return svg;
+	}
+	private static void startClip(SvgFile svg) {
+		svg.print("<clipPath id=\"clip\"><path d=\"M "+BIG_MARGIN+" "+SMALL_MARGIN+" "+
+				(SCALE-SMALL_MARGIN)+" "+SMALL_MARGIN+" "+
+				(SCALE-SMALL_MARGIN)+" "+(SCALE-BIG_MARGIN)+" "+
+				BIG_MARGIN+" "+(SCALE-BIG_MARGIN)+"\"/>"+
+				"</clipPath><g clip-path=\"url(#clip)\">");
+	}
+	private static void endClip(SvgFile svg) {
+		svg.print("</g>");
 	}
 
 }
